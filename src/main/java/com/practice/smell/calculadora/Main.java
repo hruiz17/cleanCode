aa6519ddf2383e761f1d0a6652dc31f3320e29bb
H <hruiz17@hotmail.com>
2023-08-31 18:26:41 -0500

    adición de semll

--- src/main/java/com/practice/smell/calculadora/Main.java
+++ src/main/java/com/practice/smell/calculadora/Main.java
@@ -4,5 +4,8 @@ public class Main {
     public static void main(String[] args) {
         Calculadora c = new Calculadora();
         System.out.println("Resultado: " + c.calcular(5.0,4.0));
+
+        Calculadora d = new Calculadora();
+        System.out.println("Resultado: " + d.calcular(10.0,8.0));
     }
 }
